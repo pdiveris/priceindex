fe50cc40c5ae7b9c200b447f49497303d382630b
petrosdiveris <petros@diveris.org>
2023-12-09 20:36:14 +0000

    develop: Add Products migration

---
+++ database/migrations/2023_12_09_202251_create_products.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')
+                ->nullable(false);
+
+            $table->unsignedBigInteger('category')
+                ->nullable(false);
+
+            $table->enum(
+                'unit',
+                [
+                    'weight',
+                    'quantity',
+                ]
+            );
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
