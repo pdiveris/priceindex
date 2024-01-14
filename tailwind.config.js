ffd4473b7fb8caa1fc2fbd6f1e365056eec635c5
petrosdiveris <petros@diveris.org>
2024-01-14 19:11:40 +0000

    tailwind/landwind: Basic 0.1 landing pages with Tailwind etc. Light mode only at the time

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    // darkMode: "class",
+    darkMode: "class",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -18,5 +19,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
